4988b26fe2bd15324e203f78cd1ab497931f1822
Ian Lessing <ilessing@library.ucsb.edu>
2019-02-22 16:22:58 -0800

    end of Mary's dplyr lesson

--- dplyr_fun.R
+++ dplyr_fun.R
@@ -17,3 +17,41 @@ year_country_gdp_africa <- gapminder %>% filter(continent == "Africa") %>% selec
 gdp_by_continent <- gapminder %>% 
   group_by(continent) %>%
   summarize(mean_gdp = mean(gdpPercap))
+
+# calc life expectancy per country
+lifetime_by_country <- gapminder %>%
+  group_by(country) %>% 
+  summarize(mean_life = mean(lifeExp))
+lifetime_by_country
+
+# max and min
+lifetime_by_country %>%  filter(mean_life == min(mean_life))
+lifetime_by_country %>%  filter(mean_life == max(mean_life))
+
+# in dplyr sort is arrange()
+lifetime_by_country %>% arrange(mean_life)  # ascending order
+lifetime_by_country %>% arrange(desc(mean_life))
+
+# use count() to find number of rows
+gapminder %>% 
+  filter(year == 1952) %>% 
+  count(continent)
+
+unique(gapminder$year)
+unique(gapminder$continent)
+unique(gapminder$gdpPercap)
+
+# perform more than one function inside summarize
+gapminder %>% 
+  group_by(continent) %>% 
+  summarize(
+    mean_life = mean(lifeExp),
+    se_life = sd(lifeExp)/sqrt(n())
+  )
+
+# using mutate
+gdp_pop_summary <- gapminder %>% 
+  mutate(gdp_billions = gdpPercap*pop/10^9) %>% 
+  group_by(continent,year) %>% 
+  summarize(mean_gdp_bill = mean(gdp_billions))
+head(gdp_pop_summary)
